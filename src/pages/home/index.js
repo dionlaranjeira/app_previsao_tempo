26648f3386f5914f746068fba8f437c7a2323455
Dion Laranjeira <dionribeiro.rr@gmail.com>
2021-12-02 10:49:57 -0400

    Estilização botão Procurar Cidade

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,124 +1,142 @@
-import React, { useState } from 'react'
-import {Button,StyleSheet,Text,View, Image,TextInput ,SafeAreaView, FlatList, TouchableOpacity, StatusBar} from 'react-native'
+import React, {useState} from 'react';
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  SafeAreaView,
+  FlatList,
+  TouchableOpacity,
+  StatusBar,
+} from 'react-native';
+import {InputFindCity} from '../../components/InputFindCity';
 import API from '../../service/api';
 
-export default function Home(){
+export default function Home() {
+  const [cityName, setCityName] = useState('');
+  const [citys, setCitys] = useState([]);
+  const [forecast, setForecast] = useState([]);
+  const [showForecast, setShowForecast] = useState(false);
 
-    const [cityName, setCityName] = useState("");
-    const [citys, setCitys] = useState([]);
-    const [forecast, setForecast] = useState([]);
-    const [showForecast, setShowForecast] = useState(false);
-
-    function haveForecast(){  
-      if (forecast){
-        setShowForecast(true);
-      }
-      else{setShowForecast(false)};
+  function haveForecast() {
+    if (forecast) {
+      setShowForecast(true);
+    } else {
+      setShowForecast(false);
     }
+  }
 
-    async function getCitys(cidade) {
-        const response = await API.getCityByName(cidade);
-        setCitys(response);
-        console.warn(citys);
-    }
+  async function getCitys(cidade) {
+    const response = await API.getCityByName(cidade);
+    setCitys(response);
+    console.warn(citys);
+  }
 
+  async function getForecast(idCidade) {
+    setSelectedId(idCidade);
+    const response = await API.getForecastById(idCidade);
+    setForecast(response);
+    haveForecast();
+  }
 
-    async function getForecast(idCidade){
-      setSelectedId(idCidade);
-      const response = await API.getForecastById(idCidade);
-      setForecast(response);
-      haveForecast();
-    }
+  const Item = ({item, onPress, backgroundColor, textColor}) => (
+    <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
+      <Text style={[styles.title, textColor]}>
+        {item.name} {item.country}
+      </Text>
+    </TouchableOpacity>
+  );
 
-    const Item = ({ item, onPress, backgroundColor, textColor }) => (
-        <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
-          <Text style={[styles.title, textColor]}>{item.name} {item.country}</Text>
-        </TouchableOpacity>
-      );
+  const [selectedId, setSelectedId] = useState(null);
 
-    const [selectedId, setSelectedId] = useState(null);
+  const renderItem = ({item}) => {
+    const backgroundColor = item.id === selectedId ? '#6e3b6e' : '#f9c2ff';
+    const color = item.id === selectedId ? 'white' : 'black';
 
-    
-    const renderItem = ({ item }) => {
-        const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#f9c2ff";
-        const color = item.id === selectedId ? 'white' : 'black';
-    
-        return (
-          <Item
-            item={item}
-            // onPress={() => setSelectedId(item.id)}
-            onPress={() => getForecast(item.id)}
-            backgroundColor={{ backgroundColor }}
-            textColor={{ color }}
-          />
-        );
-      };
-    
-          
+    return (
+      <Item
+        item={item}
+        // onPress={() => setSelectedId(item.id)}
+        onPress={() => getForecast(item.id)}
+        backgroundColor={{backgroundColor}}
+        textColor={{color}}
+      />
+    );
+  };
 
+  function teste() {
+    console.warn('Clicado!');
+  }
 
-    function teste(){
-        console.warn("Clicado!");
-    }
+  return (
+    <SafeAreaView style={styles.container}>
+      {/* <Image style={{width:"100%", height:"100%"}} source={require('../../assets/logo_linux_fundo.jpeg')} /> */}
 
+      <TextInput
+        style={styles.fildFindCity}
+        onChangeText={setCityName}
+        value={cityName}
+        placeholder="Pesquise por uma cidade"
+      />
 
+      <Button
+        style={styles.btnFind}
+        onPress={() => getCitys(cityName)}
+        title="Pesquisar"
+        color="#841584"
+        accessibilityLabel="Pesquise por uma cidade"
+      />
 
-    return(
-        <SafeAreaView>
-            {/* <Image style={{width:"100%", height:"100%"}} source={require('../../assets/logo_linux_fundo.jpeg')} /> */}
-            
-            <TextInput
-            // style={styles.input}
-            onChangeText={setCityName}
-            value={cityName}
-            placeholder="Pesquise por uma cidade"
-            keyboardType="numeric"
-            />
-            <Button
-                onPress={()=>getCitys(cityName)} 
-                title="TESTE"
-                color="#841584"
-                accessibilityLabel="Pesquise por uma cidade"
-            />
-
-       <FlatList
+      <FlatList
         data={citys}
         renderItem={renderItem}
-        keyExtractor={(item) => item.id}
+        keyExtractor={item => item.id}
         extraData={selectedId}
       />
 
-      {
-        showForecast && (
-      <>  
-
-      
-      <Text>Previsão para a semana</Text>
-      <View>
-        <Text>Icone clima </Text>
-        <Text>Dia </Text>
-        <Text>Temperatura</Text>
-        <Text>Vento </Text>
-        <Text>clouds 33% </Text>
-      </View>
-        </>)
-}
-        </SafeAreaView>
-      
-    );
+      {showForecast && (
+        <>
+          <Text>Previsão para a semana</Text>
+          <View>
+            <Text>Icone clima </Text>
+            <Text>Dia </Text>
+            <Text>Temperatura</Text>
+            <Text>Vento </Text>
+            <Text>clouds 33% </Text>
+          </View>
+        </>
+      )}
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      marginTop: StatusBar.currentHeight || 0,
-    },
-    item: {
-      padding: 20,
-      marginVertical: 8,
-      marginHorizontal: 16,
-    },
-    title: {
-      fontSize: 32,
-    },
-  });
+  container: {
+    flex: 1,
+    padding: 8,
+    marginTop: StatusBar.currentHeight || 0,
+    backgroundColor: '#841584',
+  },
+  item: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+
+  fildFindCity: {
+    marginBottom: 16,
+    borderColor: '#fff',
+    borderWidth: 1,
+    color: '#fff',
+    borderRadius: 8,
+    padding: 8,
+  },
+  btnFind: {
+    backgroundColor: '#f00',
+  },
+});
--- src/service/api.js
+++ src/service/api.js
@@ -1,37 +1,36 @@
-const ip ='http://192.168.0.104:5000/'
-class API{
-    async getCityByName(cidade) {
-        try {
-          const repoCall = await fetch(`${ip}/cidade/${cidade}`, {
-            method: 'GET',
-            headers: {
-              charset: 'utf-8',
-              'Content-Type': 'application/json; charset=utf-8',
-            },
-          });
-          const response = await repoCall.json();
-          return response;
-        } catch (error) {
-          console.warn("ERROR!-->"+error)
-        }
-      }
+const ip = 'http://10.0.1.94:5000/';
+class API {
+  async getCityByName(cidade) {
+    try {
+      const repoCall = await fetch(`${ip}/cidade/${cidade}`, {
+        method: 'GET',
+        headers: {
+          charset: 'utf-8',
+          'Content-Type': 'application/json; charset=utf-8',
+        },
+      });
+      const response = await repoCall.json();
+      return response;
+    } catch (error) {
+      console.warn('ERROR!-->' + error);
+    }
+  }
 
-      async getForecastById(IDcidade) {
-        try {
-          const repoCall = await fetch(`${ip}/previsao_tempo/${IDcidade}`, {
-            method: 'GET',
-            headers: {
-              charset: 'utf-8',
-              'Content-Type': 'application/json; charset=utf-8',
-            },
-          });
-          const response = await repoCall.json();
-          return response;
-        } catch (error) {
-          console.warn("ERROR!-->"+error)
-        }
-      }
-
+  async getForecastById(IDcidade) {
+    try {
+      const repoCall = await fetch(`${ip}/previsao_tempo/${IDcidade}`, {
+        method: 'GET',
+        headers: {
+          charset: 'utf-8',
+          'Content-Type': 'application/json; charset=utf-8',
+        },
+      });
+      const response = await repoCall.json();
+      return response;
+    } catch (error) {
+      console.warn('ERROR!-->' + error);
+    }
+  }
 }
 
 export default new API();
